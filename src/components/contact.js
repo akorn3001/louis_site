ed4d61595b1c0742dabec2244bd083956342af1d
Alex Kornfeld <akorn3000@gmail.com>
2018-03-20 20:27:30 -0400

    Change contact page to include an embedded google form

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,15 +6,23 @@ class Contact extends Component {
     return (
       <div className="contact-container">
         <h3>CONTACT</h3>
-        <div className="contact-img-container">
-          <img src={Airplane} alt="airplane"/>
-        </div>
-        <span>
-          Send me an email at louiskornfeld@gmail.com.
-        </span>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwhCqbDFgIZQvmPs0Q_7jQVubw-ko_d4-8XYK1ulGZClCJkA/viewform?embedded=true"
+          className="google-form"
+          title="contact-form"
+          width="760" height="1000" frameBorder="0"
+          marginHeight="0" marginWidth="0">Loading...
+        </iframe>
       </div>
     );
   }
 }
 
 export default Contact;
+
+
+// <div className="contact-img-container">
+//   <img src={Airplane} alt="airplane"/>
+// </div>
+// <span>
+//   Send me an email at louiskornfeld@gmail.com.
+// </span>
